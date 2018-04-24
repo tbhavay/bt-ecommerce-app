8b3477bdc88dc78bdba81ad7fc21cfc5c2c5bc00
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-04-24 14:48:30 -0400

    Creating ecommerce app with Udemy tutorial

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { browserHistory, Router, Route } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
+import reducers from 'reducers';
+
+
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
